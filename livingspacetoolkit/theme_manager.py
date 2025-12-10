ce499c1f0f67b8fdedeac1f00212a67a8df4e56d
Chris McGlown <cmcglown@protonmail.com>
2025-12-09 23:18:02 -0500

    Added results UI

--- livingspacetoolkit/theme_manager.py
+++ livingspacetoolkit/theme_manager.py
@@ -70,6 +70,7 @@ def is_dark_mode():
 
 def apply_theme(app: QApplication):
     """Loads light or dark QSS depending on OS settings."""
+    # TODO: Set first option to theme_dark.qss. Figure out how to properly switch between themes.
     theme_file = "Resource/theme_light.qss" if is_dark_mode() else "Resource/theme_light.qss"
 
     if os.path.exists(theme_file):
---
+++ livingspacetoolkit/views/results_view.py
@@ -0,0 +1,37 @@
+import logging
+
+from PySide6.QtCore import Qt
+from PySide6.QtGui import QFont
+from PySide6.QtWidgets import QWidget, QLabel, QPushButton, QVBoxLayout, QTextBrowser, QSpacerItem, QSizePolicy
+
+logger = logging.getLogger(__name__)
+
+class Results(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        layout: QVBoxLayout = QVBoxLayout()
+
+        spacer: QSpacerItem = QSpacerItem(20, 40, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
+
+        self.calculate_button: QPushButton = QPushButton("Calculate")
+        self.results_label: QLabel = QLabel()
+        self.results_view: QTextBrowser = QTextBrowser()
+        font: QFont = QFont("Noto Sans", 12)
+        font.setBold(True)
+        font.setItalic(True)
+        font.setUnderline(True)
+
+        self.results_label.setText("Results:")
+        self.results_label.setFont(font)
+        self.results_label.setAlignment(Qt.AlignmentFlag.AlignHCenter)
+
+        self.results_view.setMaximumSize(300, 500)
+        self.results_view.setMinimumSize(300, 500)
+
+        layout.addSpacerItem(spacer)
+        layout.addWidget(self.calculate_button)
+        layout.addWidget(self.results_label)
+        layout.addWidget(self.results_view)
+        layout.addSpacerItem(spacer)
+        self.setLayout(layout)
