06660d07c10dc18d75d6de2ed55588dc866a7851
Chris McGlown <cmcglown@protonmail.com>
2025-12-17 22:45:17 -0500

    Added test files for ui components

---
+++ tests/gui/test_roof_view.py
@@ -0,0 +1,3 @@
+import pytest
+
+# TODO: Test UI/UX for the roof view. Pitch(s), Overhang, Roofing Type, Thickness, End Cuts, Fascia
---
+++ tests/gui/test_wall_view.py
@@ -0,0 +1,4 @@
+import pytest
+
+# TODO: Test input behavior for wall heights. Use following scenarios to test all input: Wall Height & Peak Height,
+#  Max Height & Pitch, Soffit Height & Peak Height, Drip Edge & Peak Height
